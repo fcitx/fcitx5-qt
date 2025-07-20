fcitx/fcitx5-qt

commit 31caa7704243335a36a1b871907c41c5144e9bf0

drop qt5 quickphrase editor and port to StandardPaths

qt5/widgetsaddons/fcitxqtconfiguifactory.cpp
@@ -7,14 +7,14 @@
 #include "fcitxqtconfiguifactory.h"
 #include "fcitxqtconfiguifactory_p.h"
 #include "fcitxqtconfiguiplugin.h"
-
 #include <QDebug>
 #include <QDir>
 #include <QLibrary>
 #include <QPluginLoader>
 #include <QStandardPaths>
+#include <QString>
 #include <fcitx-utils/i18n.h>
-#include <fcitx-utils/standardpath.h>
+#include <fcitx-utils/standardpaths.h>
 
 namespace fcitx {
 
@@ -50,12 +50,12 @@ FcitxQtConfigUIWidget *FcitxQtConfigUIFactory::create(const QString &file) {
     Q_D(FcitxQtConfigUIFactory);
 
     auto path = normalizePath(file);
-    auto loader = d->plugins_.value(path);
+    auto *loader = d->plugins_.value(path);
     if (!loader) {
         return nullptr;
     }
 
-    auto instance =
+    auto *instance =
         qobject_cast<FcitxQtConfigUIFactoryInterface *>(loader->instance());
     if (!instance) {
         return nullptr;
@@ -71,39 +71,27 @@ bool FcitxQtConfigUIFactory::test(const QString &file) {
 }
 
 void FcitxQtConfigUIFactoryPrivate::scan() {
-    fcitx::StandardPath::global().scanFiles(
-        fcitx::StandardPath::Type::Addon, "qt5",
-        [this](const std::string &path, const std::string &dirPath, bool user) {
-            do {
-                if (user) {
-                    break;
-                }
+    auto addonFiles = fcitx::StandardPaths::global().locate(
+        fcitx::StandardPathsType::Addon, "qt6",
+        [](const std::filesystem::path &path) {
+            return QLibrary::isLibrary(QString::fromStdString(path));
+        },
+        StandardPathsMode::System);
 
-                QDir dir(QString::fromLocal8Bit(dirPath.c_str()));
-                QFileInfo fi(
-                    dir.filePath(QString::fromLocal8Bit(path.c_str())));
-
-                QString filePath = fi.filePath(); // file name with path
-                QString fileName = fi.fileName(); // just file name
-
-                if (!QLibrary::isLibrary(filePath)) {
-                    break;
-                }
-
-                QPluginLoader *loader = new QPluginLoader(filePath, this);
-                if (loader->metaData().value("IID") !=
-                    QLatin1String(FcitxQtConfigUIFactoryInterface_iid)) {
-                    delete loader;
-                    break;
-                }
-                auto metadata = loader->metaData().value("MetaData").toObject();
-                auto files = metadata.value("files").toVariant().toStringList();
-                auto addon = metadata.value("addon").toVariant().toString();
-                for (const auto &file : files) {
-                    plugins_[addon + "/" + file] = loader;
-                }
-            } while (0);
-            return true;
-        });
+    for (const auto &[_, filePath] : addonFiles) {
+        auto *loader =
+            new QPluginLoader(QString::fromStdString(filePath), this);
+        if (loader->metaData().value("IID") !=
+            QLatin1String(FcitxQtConfigUIFactoryInterface_iid)) {
+            delete loader;
+            continue;
+        }
+        auto metadata = loader->metaData().value("MetaData").toObject();
+        auto files = metadata.value("files").toVariant().toStringList();
+        auto addon = metadata.value("addon").toVariant().toString();
+        for (const auto &file : files) {
+            plugins_[addon + "/" + file] = loader;
+        }
+    }
 }
 } // namespace fcitx

qt6/widgetsaddons/fcitxqtconfiguifactory.cpp
@@ -7,14 +7,17 @@
 #include "fcitxqtconfiguifactory.h"
 #include "fcitxqtconfiguifactory_p.h"
 #include "fcitxqtconfiguiplugin.h"
-
 #include <QDebug>
 #include <QDir>
+#include <QLatin1String>
 #include <QLibrary>
+#include <QObject>
 #include <QPluginLoader>
 #include <QStandardPaths>
+#include <QtClassHelperMacros>
 #include <fcitx-utils/i18n.h>
-#include <fcitx-utils/standardpath.h>
+#include <fcitx-utils/standardpaths.h>
+#include <filesystem>
 
 namespace fcitx {
 
@@ -50,12 +53,12 @@ FcitxQtConfigUIWidget *FcitxQtConfigUIFactory::create(const QString &file) {
     Q_D(FcitxQtConfigUIFactory);
 
     auto path = normalizePath(file);
-    auto loader = d->plugins_.value(path);
+    auto *loader = d->plugins_.value(path);
     if (!loader) {
         return nullptr;
     }
 
-    auto instance =
+    auto *instance =
         qobject_cast<FcitxQtConfigUIFactoryInterface *>(loader->instance());
     if (!instance) {
         return nullptr;
@@ -71,39 +74,27 @@ bool FcitxQtConfigUIFactory::test(const QString &file) {
 }
 
 void FcitxQtConfigUIFactoryPrivate::scan() {
-    fcitx::StandardPath::global().scanFiles(
-        fcitx::StandardPath::Type::Addon, "qt6",
-        [this](const std::string &path, const std::string &dirPath, bool user) {
-            do {
-                if (user) {
-                    break;
-                }
+    auto addonFiles = fcitx::StandardPaths::global().locate(
+        fcitx::StandardPathsType::Addon, "qt6",
+        [](const std::filesystem::path &path) {
+            return QLibrary::isLibrary(QString::fromStdString(path));
+        },
+        StandardPathsMode::System);
 
-                QDir dir(QString::fromLocal8Bit(dirPath.c_str()));
-                QFileInfo fi(
-                    dir.filePath(QString::fromLocal8Bit(path.c_str())));
-
-                QString filePath = fi.filePath(); // file name with path
-                QString fileName = fi.fileName(); // just file name
-
-                if (!QLibrary::isLibrary(filePath)) {
-                    break;
-                }
-
-                QPluginLoader *loader = new QPluginLoader(filePath, this);
-                if (loader->metaData().value("IID") !=
-                    QLatin1String(FcitxQtConfigUIFactoryInterface_iid)) {
-                    delete loader;
-                    break;
-                }
-                auto metadata = loader->metaData().value("MetaData").toObject();
-                auto files = metadata.value("files").toVariant().toStringList();
-                auto addon = metadata.value("addon").toVariant().toString();
-                for (const auto &file : files) {
-                    plugins_[addon + "/" + file] = loader;
-                }
-            } while (0);
-            return true;
-        });
+    for (const auto &[_, filePath] : addonFiles) {
+        auto *loader =
+            new QPluginLoader(QString::fromStdString(filePath), this);
+        if (loader->metaData().value("IID") !=
+            QLatin1String(FcitxQtConfigUIFactoryInterface_iid)) {
+            delete loader;
+            continue;
+        }
+        auto metadata = loader->metaData().value("MetaData").toObject();
+        auto files = metadata.value("files").toVariant().toStringList();
+        auto addon = metadata.value("addon").toVariant().toString();
+        for (const auto &file : files) {
+            plugins_[addon + "/" + file] = loader;
+        }
+    }
 }
 } // namespace fcitx

qt5/CMakeLists.txt
@@ -11,6 +11,5 @@ add_subdirectory(platforminputcontext)
 if(NOT BUILD_ONLY_PLUGIN)
   add_subdirectory(guiwrapper)
   add_subdirectory(widgetsaddons)
-  add_subdirectory(quickphrase-editor)
   add_subdirectory(immodule-probing)
 endif()

qt5/guiwrapper/wrapperapp.cpp
@@ -5,22 +5,24 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
-#include <QDebug>
-
-#include "fcitxqtconfiguifactory.h"
-#include "mainwindow.h"
 #include "wrapperapp.h"
+#include "fcitxqtconfiguifactory.h"
+#include "fcitxqtconfiguiwidget.h"
+#include "mainwindow.h"
+#include <QApplication>
 #include <QCommandLineParser>
+#include <QDebug>
+#include <QLatin1String>
 #include <QWindow>
+#include <Qt>
+#include <QtGlobal>
 #include <fcitx-utils/i18n.h>
-#include <fcitx-utils/standardpath.h>
-#include <qnamespace.h>
 
 namespace fcitx {
 
 WrapperApp::WrapperApp(int &argc, char **argv)
     : QApplication(argc, argv), factory_(new FcitxQtConfigUIFactory(this)),
-      mainWindow_(0) {
+      mainWindow_(nullptr) {
     setApplicationName(QLatin1String(
         "fcitx5-qt" QT_STRINGIFY(QT_VERSION_MAJOR) "-gui-wrapper"));
     setApplicationVersion(QLatin1String(FCITX5_QT_VERSION));
@@ -79,11 +81,7 @@ void WrapperApp::init() {
 
 void WrapperApp::run() { mainWindow_->show(); }
 
-WrapperApp::~WrapperApp() {
-    if (mainWindow_) {
-        delete mainWindow_;
-    }
-}
+WrapperApp::~WrapperApp() { delete mainWindow_; }
 
 void WrapperApp::errorExit() { exit(1); }
 } // namespace fcitx

qt5/quickphrase-editor/CMakeLists.txt
@@ -1,26 +1,0 @@
-find_package(Qt5Concurrent REQUIRED)
-
-set(QUICKPHRASE_EDITOR_SRCS 
-  main.cpp
-  model.cpp
-  editor.cpp
-  editordialog.cpp
-  batchdialog.cpp
-  filelistmodel.cpp
-  )
-
-add_library(fcitx-quickphrase-editor5-qt5
-  MODULE ${QUICKPHRASE_EDITOR_SRCS})
-set_target_properties(fcitx-quickphrase-editor5-qt5 PROPERTIES
-                      OUTPUT_NAME fcitx-quickphrase-editor5
-                      AUTOMOC TRUE AUTOUIC TRUE AUTOUIC_OPTIONS "-tr=fcitx::tr2fcitx;--include=fcitxqti18nhelper.h"
-)
-target_link_libraries(fcitx-quickphrase-editor5-qt5
-  Fcitx5::Utils
-  Qt5::Core
-  Qt5::Gui
-  Qt5::Concurrent
-  Fcitx5Qt5::WidgetsAddons
-)
-
-install(TARGETS fcitx-quickphrase-editor5-qt5 DESTINATION ${CMAKE_INSTALL_LIBDIR}/fcitx5/qt5)

qt5/quickphrase-editor/batchdialog.cpp
@@ -1,1 +1,0 @@
-../../qt6/quickphrase-editor/batchdialog.cpp

qt5/quickphrase-editor/batchdialog.h
@@ -1,1 +1,0 @@
-../../qt6/quickphrase-editor/batchdialog.h

qt5/quickphrase-editor/batchdialog.ui
@@ -1,1 +1,0 @@
-../../qt6/quickphrase-editor/batchdialog.ui

commit 83a23312cf8a98a316f681676fa7c650970ba90b

qt5: fix qt5 gui-wrapper does not work
Fixes: 31caa7704243 ("drop qt5 quickphrase editor and port to StandardPaths")

qt5/widgetsaddons/fcitxqtconfiguifactory.cpp
@@ -72,7 +72,7 @@ bool FcitxQtConfigUIFactory::test(const QString &file) {
 
 void FcitxQtConfigUIFactoryPrivate::scan() {
     auto addonFiles = fcitx::StandardPaths::global().locate(
-        fcitx::StandardPathsType::Addon, "qt6",
+        fcitx::StandardPathsType::Addon, "qt5",
         [](const std::filesystem::path &path) {
             return QLibrary::isLibrary(QString::fromStdString(path));
         },